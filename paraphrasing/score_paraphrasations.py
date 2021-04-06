8dc857bb9fe2210559e42e08693f80581a739e49
54u <80169290+54u@users.noreply.github.com>
2021-04-06 14:45:50 +0200

    include headers and labels in the mixed_paraphrasations.csv

--- paraphrasing/score_paraphrasations.py
+++ paraphrasing/score_paraphrasations.py
@@ -30,10 +30,12 @@ def paraphrasation_score(original: str, paraphrasation: str) -> int:
 
 
 def write_maxima(maxima: Dict[str, List[Tuple[str, str, float]]],
-                 original_data: Dict[str, List[str]]):
+                 original_data: Dict[str, List[str]],
+                 header: List[str]):
     meta: Dict[str, List[str]] = {}
     with open("cloze_test.mixed_paraphrased.csv", "w") as outfile:
         csv_writer = csv.writer(outfile)
+        csv_writer.writerow(header)
         for key, entries in maxima.items():
             paras, languages, scores = zip(*entries)
             scores = [int(score) for score in scores]
@@ -45,11 +47,10 @@ def write_maxima(maxima: Dict[str, List[Tuple[str, str, float]]],
 
 
 def uuid_dict(rows: Iterable[Iterable[str]]) -> Dict[str, List[str]]:
-    return {row[0]: [cell
-                     for cell in row[1:]
-                     if not is_parsable(cell, int)]
-            for row in rows
-            if len(row) and is_parsable(row[0], UUID)}
+    return next(rows), {row[0]: [cell
+                                 for cell in row[1:]]
+                        for row in rows
+                        if len(row) and is_parsable(row[0], UUID)}
 
 
 def score_paraphrasations(dataset: str = "cloze_test",
@@ -71,13 +72,13 @@ def score_paraphrasations(dataset: str = "cloze_test",
     maxima: Dict[str, List[Tuple[str, str, float]]] = {}
     with open(original_path) as original_file:
         reader = csv.reader(original_file)
-        original_data = uuid_dict(reader)
+        header, original_data = uuid_dict(reader)
     for languages in (lang_pipelines):
         print("Evaluating paraphrasation score of", tuple(languages[:-1]))
         path = os.path.join(wd, f"{dataset}.{'_'.join(languages)}.csv")
         with open(path) as para_file:
             reader = csv.reader(para_file)
-            para_data = uuid_dict(reader)
+            _, para_data = uuid_dict(reader)
         scores = []
         for key, para_cells in tqdm(para_data.items()):
             if key not in original_data:
@@ -92,6 +93,8 @@ def score_paraphrasations(dataset: str = "cloze_test",
             if key not in maxima:
                 maxima[key] = []
             for i, para in enumerate(para_cells):
+                if i >= STRINGS_PER_STORY:
+                    continue
                 score = paraphrasation_score(original_cells[i], para)
                 scores.append(score)
                 if len(maxima[key]) <= i:
@@ -99,8 +102,9 @@ def score_paraphrasations(dataset: str = "cloze_test",
                 elif maxima[key][i][2] < score:
                     maxima[key][i] = (para, tuple(languages[:-1]), score)
             assert len(maxima[key]) == STRINGS_PER_STORY
-            assert len(para_cells) == STRINGS_PER_STORY
-            assert len(original_cells) == STRINGS_PER_STORY
+            assert len(para_cells) == STRINGS_PER_STORY + 1
+            assert len(original_cells) == STRINGS_PER_STORY + 1
+            assert original_cells[-1] == para_cells[-1]
         this_stats = pd.DataFrame({tuple(languages[:-1]):
                                    scores}).describe().T
         print(this_stats)
@@ -119,7 +123,7 @@ def score_paraphrasations(dataset: str = "cloze_test",
                   for para, languages, score in values]
     }).describe()
     merged.to_csv("merged_score.csv")
-    write_maxima(maxima, original_data)
+    write_maxima(maxima, original_data, header)
     return statistics, merged
 
 
